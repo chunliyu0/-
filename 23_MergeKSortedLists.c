7a2ec310bb76fdcb1ca4748031dc2b2c66fad768
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-05 01:11:41 -0400

    Update 23_MergeKSortedLists.c

--- 23_MergeKSortedLists.c
+++ 23_MergeKSortedLists.c
@@ -1,3 +1,5 @@
+/* Merge Sort because we can already merge two sorted lists*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -6,46 +8,44 @@
  * };
  */
  
-struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
-    if (!l1) return l2;
-    if (!l2) return l1;
-    struct ListNode *head, *f, *s, *p;
-    if (l1->val < l2->val) {
-        head = l1;
-        f = l1->next;
-        s = l2;
+ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+     //Base case
+    if(l1 == NULL)return l2;
+    if(l2==NULL)return l1;
+    
+    struct ListNode *ret;
+    
+    //recursive step
+    if( l1->val < l2->val){
+        ret=l1;   
+        ret->next=mergeTwoLists(l1->next,l2);
     }
-    else {
-        head = l2;
-        f = l1;
-        s = l2->next;
-    }
-    p = head;
-    for (; f && s;) {
-        if (f->val < s->val) {
-            p->next = f;
-            f = f->next;
-        }
-        else {
-            p->next = s;
-            s = s->next;
-        }
-        p = p->next;
-    }
-    if (f) p->next = f;
-    else p->next = s;
-    return head;
+    else{ 
+        ret=l2;   
+        ret->next = mergeTwoLists(l1,l2->next);
+    }  
+    return ret; 
 }
+
 struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
-    if (!listsSize) return 0;
-    if (listsSize == 1) return lists[0];
-    int i, id = 0;
-    for (i = 0; i < listsSize; ) {
-        if (i + 1 < listsSize) {
-            lists[id++] = mergeTwoLists(lists[i], lists[i + 1]);
-            i += 2;
+    //critical condition
+    if(listsSize == 0)
+        return NULL;
+    
+    int begin, end = listsSize-1;
+    
+    while(end>0){
+        begin = 0;
+        while(begin < end){
+            lists[begin] = mergeTwoLists(lists[begin], lists[end]);
+            begin++;
+            end--;
         }
-        else lists[id++] = lists[i++];
     }
-    return mergeKLists(lists, id);
+    
+    return lists[0];
 }
+
+
+
+
