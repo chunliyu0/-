6ceeb4ec05aef4d2e134bfb569b87e8039ace08e
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-05 03:48:39 -0400

    Create 283_MoveZeros.c

---
+++ 283_MoveZeros.c
@@ -0,0 +1,14 @@
+void moveZeroes(int* nums, int numsSize) {
+    if(numsSize <2)return;
+        
+    int i, j = 0;//j points to zeroes from the beginning, i tries to find nonzeros in order.
+    int temp;
+    //if i == j, and nums[i]!=0, j will go one step further after switch
+    for (i = 0; i < numsSize; i++) {
+          if (nums[i] != 0) {
+              temp = nums[i];
+              nums[i] = nums[j];
+              nums[j++] = temp;
+            }
+        }
+}
