f191f936c68a7a1682e5f1afc1cb19803c85b20b
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-05 02:55:05 -0400

    Update 26_RemoveDuplicates.c

--- 26_RemoveDuplicates.c
+++ 26_RemoveDuplicates.c
@@ -4,7 +4,6 @@ int removeDuplicates(int* nums, int numsSize) {
     if(numsSize<2)return numsSize;
     
     //count is to save the distinguished number while i is used to traverse the array.
-    record the last non-duplicate position;
     int count = 0, i;
     for(i=1;i<numsSize;i++){
         if(nums[i]!=nums[i-1]){
