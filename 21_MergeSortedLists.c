a3ccd3339a869c722ee24735206846a3680ecb2e
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-04 23:00:10 -0400

    Create 21_MergeSortedLists.c

---
+++ 21_MergeSortedLists.c
@@ -0,0 +1,33 @@
+/*save the result to another list l3 (ret)*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode *ret;
+    
+    //Base case
+    if(l1 == NULL){  
+        ret=l2;
+        return ret;
+    }
+    if(l2==NULL){ 
+        ret=l1;
+        return ret;
+    }
+    
+    //recursive step
+    if( l1->val < l2->val){
+        ret=l1;   
+        ret->next=mergeTwoLists(l1->next,l2);
+    }
+    else{ 
+        ret=l2;   
+        ret->next = mergeTwoLists(l1,l2->next);
+    }  
+    return ret; 
+}
