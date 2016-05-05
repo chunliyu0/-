1e1e5cbff44202064e73849181cf257a96ca8567
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-05 04:11:33 -0400

    Update 283_MoveZeros.c

--- 283_MoveZeros.c
+++ 283_MoveZeros.c
@@ -1,14 +1,18 @@
 void moveZeroes(int* nums, int numsSize) {
     if(numsSize < 2)return;
         
-    int i, j = 0;//j points to zeroes from the beginning, i tries to find nonzeros in order.
-    int temp;
-    //if i == j, and nums[i]!=0, j will go one step further after switch
+    //i traverses the array and tries to find nonzeros while j points to the 1st zero spot.
+    int i, j = 0, temp;
+
+    //if i == j nothing will be changed with the element swapping with itself
+    //else i will move faster and the gap between i and j are 0's
     for (i = 0; i < numsSize; i++) {
           if (nums[i] != 0) {
+              //Swap nums[i] and nums[j]
               temp = nums[i];
               nums[i] = nums[j];
-              nums[j++] = temp;
+              nums[j] = temp;
+              j++;
             }
         }
 }
