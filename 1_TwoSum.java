bbdf2de5e53f6cf31cea1f4fac1be86c90e07b7a
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-07 16:50:08 -0400

    Create 1_TwoSum.java

---
+++ 1_TwoSum.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int i;
+        
+        //the key cannot be repeated in a map. the first key will be replaced when it appears the second time
+        for(i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for(i=0;i<nums.length;i++){
+            if(map.get(target-nums[i])!=null&&map.get(target-nums[i])!=i){
+                result[0] = i+1;
+                result[1] = map.get(target-nums[i])+1;
+                break;
+            }
+        }
+       return result;
+    }
+}
