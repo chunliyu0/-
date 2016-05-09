b05382d795a9352ea5eeb6324cc8a8f0b874a8e1
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-09 02:08:40 -0400

    Update 041_FirstMissingPositive.c

--- 041_FirstMissingPositive.c
+++ 041_FirstMissingPositive.c
@@ -1,13 +1,17 @@
+/*put num[i] under the index i, only swap that need to (nums[i] != i) and can ((nums[i]>0 && nums[i]<numsSize)) be swapped;
+ *if (nums[nums[i]] == nums[i]), [1,1] will get into trouble. -> repeated numbers.
+ */
+
 void swap(int *i, int *j){
-	int temp = *i;
-	*i = *j;
-	*j = temp;
+    int temp = *i;
+    *i = *j;
+    *j = temp;
 }
 
 int firstMissingPositive(int nums[], int numsSize){
     int i=0;
     while(i<numsSize){
-        if ((nums[i]>0 && nums[i]<numsSize) && nums[i] != i && nums[nums[i]] != nums[i])   
+        if ((nums[i]>0 && nums[i]<numsSize) && nums[i] != i && nums[i]!=nums[nums[i]])   
             swap(&nums[i], &nums[nums[i]]);  
         else 
             i++;
