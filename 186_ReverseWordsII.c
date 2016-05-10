60f09f20466d5c090b90f206413aa79423ae91df
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-10 01:59:56 -0400

    Create 186_ReverseWordsII.c

---
+++ 186_ReverseWordsII.c
@@ -0,0 +1,39 @@
+/* Analysis
+(1) Reverse the whole string first -> Reverse each string
+(2) For the last string, the punctuation marks should be considered.
+
+void swap(char *a, char *b){
+    char tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void reverseWord(char s[], int i, int j){
+    char tmp;
+    while(i<j){
+        swap(&s[i], &s[j]);
+        i++;
+        j--;
+    }
+}
+
+void reverseWords(char *s) {
+    int i=0, j=0;
+    
+    //reverse the whole string
+    reverseWord(s, 0, strlen(s) - 1);
+    
+    //reverse each word
+    for(i=0, j=0; s[j]!='\0'; j++){
+        if(s[j]==' '){
+            reverseWord(s, i, j-1);
+            i = j+1;
+        }
+    }
+    
+    //reverse the last string with punctuation marks
+    if(s[j-1]=='!'||s[j-1]=='?'||s[j-1]=='.')
+        reverseWord(s, i, j-2);
+    else
+        reverseWord(s, i, j-1);
+}
