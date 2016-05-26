bd19b107ecc3b1588636d4a232343adf8bd25574
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-26 11:48:54 -0700

    Create 347_TopKFrequent.c

---
+++ 347_TopKFrequent.c
@@ -0,0 +1,32 @@
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        if(nums==null)return null;
+        
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int num: nums){
+            if(!map.containsKey(num))
+                map.put(num, 1);
+            else
+                map.put(num, map.get(num)+1);
+        }
+        
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>(map.entrySet());
+        Collections.sort(list,new Comparator<Map.Entry<Integer, Integer>>() {
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o2.getValue()-o1.getValue();
+            }
+        });
+        
+        List<Integer> ret = new ArrayList<Integer>();
+        int i =0;
+        for(Map.Entry<Integer, Integer> entry: list){
+            i++;
+            if(i<=k){
+                ret.add(entry.getKey().intValue());
+            }
+            else
+                break;
+        }
+        return ret;
+    }
+}
