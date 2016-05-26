86e1192d8981c89b9a37d793baf7d3e7008874ca
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-26 11:52:53 -0700

    Update 347_TopKFrequent.java

--- 347_TopKFrequent.java
+++ 347_TopKFrequent.java
@@ -4,8 +4,10 @@ Using HashMap and Comparator
 
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
+        //Base Case
         if(nums==null)return null;
         
+        //Find the frequency of each element
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int num: nums){
             if(!map.containsKey(num))
@@ -14,6 +16,7 @@ public class Solution {
                 map.put(num, map.get(num)+1);
         }
         
+        //Sort the map based on the frequency in decreasing order
         List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>(map.entrySet());
         Collections.sort(list,new Comparator<Map.Entry<Integer, Integer>>() {
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
@@ -21,6 +24,7 @@ public class Solution {
             }
         });
         
+        //Put the k frequent elements in the the result list
         List<Integer> ret = new ArrayList<Integer>();
         int i =0;
         for(Map.Entry<Integer, Integer> entry: list){
