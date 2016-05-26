1a4218bce24a253a87d562c47a7f856c0909c6f0
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-26 11:17:33 -0700

    Create 215_FindKthLargest3.c

---
+++ 215_FindKthLargest3.c
@@ -0,0 +1,20 @@
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        
+        for(int num: nums){
+            if(!map.containsKey(num))
+                map.put(num, 1);
+            else
+                map.put(num, map.get(num)+1);
+        }
+        
+        int sum = 0;
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            sum+= entry.getValue();
+            if(sum>=nums.length-k+1)
+                return entry.getKey();
+        }
+        return -1;
+    }
+}
