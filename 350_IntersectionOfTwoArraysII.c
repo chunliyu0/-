6e1f43e4ec6a725e0868f5386a325e61cbc984d9
ycl11761 <ycl11761@users.noreply.github.com>
2016-05-26 09:29:07 -0700

    Update 350_IntersectionOfTwoArraysII.c

--- 350_IntersectionOfTwoArraysII.c
+++ 350_IntersectionOfTwoArraysII.c
@@ -1,7 +1,16 @@
+/* Analysis
+Using a hashmap to save the first array, value is the number of num,
+when the elements in the second array are also contained in the hashmap, which means they are intersections,
+the corresponding value is decreased by 1, and the num is put to the list;
+finally, convert the list to the int[].
+*/
+
 public class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
+        //Base cases
         if(nums1 == null || nums2 == null)return null;
         
+        //save the 1st array
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int num: nums1){
             if(!map.containsKey(num))
@@ -10,6 +19,7 @@ public class Solution {
                 map.put(num, map.get(num)+1);
         }
         
+        //find the intersection in the 2nd array
         List<Integer> list=new ArrayList<Integer>();  
         for(int num: nums2){
             if(map.containsKey(num) && map.get(num)>0){
@@ -18,6 +28,7 @@ public class Solution {
             }
         }
         
+        //transform the result set to int array (note: not Integer array)
         int[] ret = new int[list.size()];       
         Iterator<Integer> iter = list.iterator();
         for (int i=0; iter.hasNext(); i++) {       
