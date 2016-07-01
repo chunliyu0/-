dbf286dbf071c6bf3d5c3f1869162f385bda58ae
Chunli Yu <ycl11761@users.noreply.github.com>
2016-06-30 19:34:30 -0700

    Create 280_WiggleSort.c

---
+++ 280_WiggleSort.c
@@ -0,0 +1,27 @@
+/* Analysis
+just check if the value at the odd index is the partial greatest one.
+*/
+
+void swap(int * n1, int * n2){
+    int tmp = *n1;
+    *n1 = *n2;
+    *n2 = tmp;
+}
+
+void wiggleSort(int* nums, int numsSize) {
+    if(nums==NULL || numsSize < 2) return;
+    for(int i = 1; i < numsSize; i+=2){
+        if(i == numsSize-1){
+            if(nums[i-1] > nums[i])
+                swap(&nums[i-1], &nums[i]);
+        }
+        else{
+            if(nums[i] >= nums[i-1] && nums[i] >= nums[i+1])
+                continue;
+            else{
+                int max = (nums[i-1] < nums[i+1]? i+1: i-1);
+                swap(&nums[max], &nums[i]);
+            }
+        }
+    }
+}
