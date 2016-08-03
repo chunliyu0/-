b8129d3c2731043e1f5d19575511823b1d451e62
Chunli Yu <ycl11761@users.noreply.github.com>
2016-08-03 00:35:11 -0700

    Update 098_ValidateBST.java

--- 098_ValidateBST.java
+++ 098_ValidateBST.java
@@ -1,5 +1,30 @@
-/* Analysis - Inorder */
+// I best soluton - Recursive
+/* Analysis - Recursive */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValid(TreeNode node, double min, double max){
+        if(node == null )return true;
+        
+        if(node.val < max && node.val > min)
+            return isValid(node.left, min, node.val) && isValid(node.right, node.val, max);
+        return false;
+    }
+    
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
+}
 
+// II Iterative Solution
+/* Analysis - Inorder */
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -42,3 +67,4 @@ public class Solution {
         return true;
     }
 }
+
