36bad4558d4e547a2ca269cd2990346bf0898391
Chunli Yu <ycl11761@users.noreply.github.com>
2016-08-14 23:12:01 -0700

    Update 279_PerfectSquares.java

--- 279_PerfectSquares.java
+++ 279_PerfectSquares.java
@@ -13,7 +13,7 @@ public class Solution {
             if(ref[i] <= 2)
                 continue;
             for(int j = 1; j*j <= i; j++){
-                ref[i] = Math.min(ref[i], 1+ref[i-j*j]);
+                ref[i] = Math.min(ref[i], 1+ref[i-j*j]);//when we calculate i, it means i-1 has been calculated
             }
         }
             
