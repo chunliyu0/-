d262c2692891fb6ce2ae22008fad27272d30fb51
Chunli Yu <ycl11761@users.noreply.github.com>
2016-08-14 23:10:39 -0700

    Create 279_PerfectSquares.java

---
+++ 279_PerfectSquares.java
@@ -0,0 +1,22 @@
+/* Analysis - DP_1D
+(1) For each number, consider all the combinations of different squares
+*/
+
+public class Solution {
+    public int numSquares(int n) {
+        int [] ref = new int[n+1];
+        for(int i = 1; i<=n; i++){//leave ref[0] == 0
+            ref[i] = n+1;
+        }
+        
+        for(int i = 1; i <= n; i++){
+            if(ref[i] <= 2)
+                continue;
+            for(int j = 1; j*j <= i; j++){
+                ref[i] = Math.min(ref[i], 1+ref[i-j*j]);
+            }
+        }
+            
+        return ref[n];
+    }
+}
