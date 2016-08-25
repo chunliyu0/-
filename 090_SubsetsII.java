fef999a37835b3d52748573bfa67a992a0bc451c
Chunli Yu <ycl11761@users.noreply.github.com>
2016-08-24 23:09:33 -0700

    Create 090_SubsetsII.java

---
+++ 090_SubsetsII.java
@@ -0,0 +1,25 @@
+/* Analysis - DFS 
+Remove the repeated ones
+*/
+
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        List<Integer> curr = new ArrayList<>();
+        dfs(ret, curr, nums, 0);
+        return new ArrayList<List<Integer>>(ret);
+    }
+    
+    public void dfs(List<List<Integer>> ret, List<Integer> curr, int[] nums, int i) {
+        if(i == nums.length){
+            ret.add(curr);
+            return;
+        }
+        List<Integer> tmp1 = new ArrayList<>(curr);
+        dfs(ret, tmp1, nums, i+1);
+        
+        List<Integer> tmp2 = new ArrayList<>(curr);
+        tmp2.add(nums[i]);
+        dfs(ret, tmp2, nums, i+1);
+    }
+}
