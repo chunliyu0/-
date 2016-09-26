3f3f818ccf38ada171fb02a9bb72b9e2335c596c
Chunli Yu <ycl11761@users.noreply.github.com>
2016-09-25 22:30:35 -0700

    Create 377_CombinationSumIV.java

---
+++ 377_CombinationSumIV.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int [] ref= new int[target + 1];
+        ref[0] = 1;
+        for(int i = 1; i<= target; i++){
+            for( int j = 0; j<nums.length; j++){//put nums[j] as the head
+                if( nums[j] <= i )
+                    ref[i] += ref[i - nums[j]]; 
+            }
+        }
+        return ref[target];
+    }
+}
